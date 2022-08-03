f5eff0234a536064c1f3fb38a187600076114bf6
ancrvlh <abraaoncrvlh@gmail.com>
2022-08-03 10:07:09 -0300

    Hello World

---
+++ helloWorld/js/index.js
@@ -0,0 +1,11 @@
+//Issue an alert without data entry
+window.alert('Hello, World !')
+
+//Prints a Hello World and asks for input
+window.prompt('Hello, World !')
+
+//Display a Hello World message on the screen
+document.write('Hello, World !')
+
+//This command will go in the browser Console
+console.log('Hello, World !')
