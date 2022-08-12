6768b37cb0dff0e61ddff10071a48d6b531968b8
ancrvlh <abraaoncrvlh@gmail.com>
2022-08-11 21:44:42 -0300

    JavaScript exercises

--- extraExercices/Calculator/js/index.js
+++ extraExercices/Calculator/js/index.js
@@ -1,7 +1,9 @@
 let n1 = document.getElementById('num1')
 let n2 = document.getElementById('num2')
 let calc = document.getElementById('done')
+let res = document.getElementById('result')
 
 function calculate() {
-  
+  let s = n1 + n2
+  alert(s)
 }
