eeb3b26ded10488b336b56289b9cd22c14932f6d
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-10-06 16:22:35 -0300

    DOM JavaScript

---
+++ DOM/eventsDOM/js/index.js
@@ -0,0 +1,17 @@
+function clicar() {
+  let a = document.getElementById('area')
+
+  a.innerText = 'Clicou >.<'
+}
+
+function entrar() {
+  let e = document.getElementById('area')
+
+  e.innerText = 'Entrou :)'
+}
+
+function sair() {
+  let s = document.getElementById('area')
+
+  s.innerText = 'Saiu :('
+}
