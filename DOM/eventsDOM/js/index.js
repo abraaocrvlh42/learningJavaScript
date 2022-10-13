1e7ece29a87b4530887317e878e3e223390be216
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-10-13 20:14:07 -0300

    DOM JavaScript

--- DOM/eventsDOM/js/index.js
+++ DOM/eventsDOM/js/index.js
@@ -2,6 +2,10 @@ let a = document.getElementById('area')
 let e = document.getElementById('area')
 let s = document.getElementById('area')
 
+a.addEventListener('click', clicar)
+a.addEventListener('enter', entrar)
+a.addEventListener('exit', sair)
+
 function clicar() {
   a.innerText = 'Clicou >.<'
 }
