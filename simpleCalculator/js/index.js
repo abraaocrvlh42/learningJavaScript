a787ffe58dc2f1315150c1f9c86b0da29a580e9a
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-10-14 20:19:40 -0300

    Simple Calculator in JavaScript

--- simpleCalculator/js/index.js
+++ simpleCalculator/js/index.js
@@ -1,6 +1,11 @@
 // Simple Calculator in JavaScript
 
+let tn1 = document.getElementById('num1')
+let tn2 = document.getElementById('num2')
+
 function sum() {
-  let tn1 = document.getElementById('num1')
-  let tn2 = document.getElementById('num2')
+  let n1 = Number(tn1.value)
+  let n2 = Number(tn2.value)
+  
+  let sum = n1 + n2
 }
