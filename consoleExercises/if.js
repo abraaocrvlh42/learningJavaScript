adbaac59eed26710b892b1d902518b1a28b63206
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-10-24 21:04:49 -0300

    Console exercises in JavaScript

---
+++ consoleExercises/if.js
@@ -0,0 +1,7 @@
+let vel = 80
+
+if (vel > 80) {
+  console.log('You exceeded the speed limit, fine!')
+} else {
+  console.log('Limit allowed, drive carefully!')
+}
--- consoleExercises/velocity.js
+++
@@ -1,7 +0,0 @@
-let vel = 60.5
-let car = 'fiat'
-let model = 'uno'
-
-console.log(`${car}`)
-console.log(`${vel}`)
-console.log(`${model}`)
