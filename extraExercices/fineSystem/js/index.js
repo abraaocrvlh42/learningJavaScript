052824bb726afaa726bed32257088a23d87e2662
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-10-24 22:12:13 -0300

    Console exercises in JavaScript

--- extraExercices/fineSystem/js/index.js
+++ extraExercices/fineSystem/js/index.js
@@ -2,4 +2,12 @@ function calculate() {
   let vel = document.getElementById('#velocity')
   let res = document.getElementById('#result')
   let calc = Number(vel.value)
+
+  result.innerHTML = `Your current speed is: ${vel}km/h`
+
+  if (vel > 60) {
+    result.innerHTML += 'Fined for speeding !'
+  }
+
+  result.innerHTML = 'Drive safely !'
 }
