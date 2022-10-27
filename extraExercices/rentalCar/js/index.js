ddae4408b7c8ffdcca9936a301d130f5411dfcb8
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-10-26 21:55:30 -0300

    Extra Exercises

--- extraExercices/rentalCar/js/index.js
+++ extraExercices/rentalCar/js/index.js
@@ -1,15 +1,15 @@
 // Rental Car
 
-let btn = document.querySelector(".btn-form");
-    let valorTotalInput = document.querySelector(".valorTotal");
+let btn = document.querySelector(".btn")
+    let valueTotalInput = document.querySelector('.amount')
 
-    btn.addEventListener("click", function () {
+    btn.addEventListener('click', function () {
       let dailyQuantity = Number(
-        document.querySelector(".dailyQuantity").value
-      );
+        document.querySelector('.dailyQuantity').value
+      )
       let valueMileageRound = Number(
         document.querySelector(".valueMileageRound").value
-      );
+      )
 
-      valorTotalInput.value = dailyQuantity + valueMileageRound;
-    });
+      valueTotalInput.value = dailyQuantity + valueMileageRound
+    })
