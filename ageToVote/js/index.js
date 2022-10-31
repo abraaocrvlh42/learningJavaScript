168a6f8f6c1c9c452d3df7257ee0b25d5c95d8de
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-10-31 20:57:33 -0300

    Age to Vote

--- ageToVote/js/index.js
+++ ageToVote/js/index.js
@@ -1,8 +1,10 @@
 let age = 18
 
+document.write(`You are ${age} old`)
+
 if (age < 16) {
   document.write('No voting.')
-} else if (age < 18) {
+} else if (age < 18 || age > 65) {
   document.write('Optional vote')
 } else {
   document.write('Mandatory vote')
