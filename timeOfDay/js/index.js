c72433b80a03bbd70881b0737799baa474ca4d0a
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-11-07 20:50:33 -0300

    time of day

--- timeOfDay/js/index.js
+++ timeOfDay/js/index.js
@@ -1,4 +1,19 @@
 // Script to manipulate date and time
-function timeOfDay() {
 
+function timeOfDay() {
+  let msg = document.querySelector('.msg')
+  let img = document.querySelector('.image')
+
+  let date = new Date()
+  let hour = date.getHours()
+
+  msg.innerHTML = `The current time is: ${hour}`
+
+  if(hour >= 0 && hour < 12) {
+
+  } else if(hour >= 12 && hour < 18) {
+
+  } else {
+    
+  }
 }
