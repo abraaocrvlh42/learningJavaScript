d767e32faf37c549ce5b4da0da7230490ca1a887
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-11-07 21:22:49 -0300

    time of day

--- timeOfDay/js/index.js
+++ timeOfDay/js/index.js
@@ -10,10 +10,13 @@ function timeOfDay() {
   msg.innerHTML = `The current time is: ${hour}`
 
   if(hour >= 0 && hour < 12) {
-
+    img.src = 'morning.jpg'
+    document.body.style.background = '#FDB48B'
   } else if(hour >= 12 && hour < 18) {
-
+    img.src = 'afternoon.jpg'
+    document.body.style.background = '#8298B0'
   } else {
-    
+    img.src = './img/night.jpg'
+    document.body.style.background = '#222324'
   }
 }
