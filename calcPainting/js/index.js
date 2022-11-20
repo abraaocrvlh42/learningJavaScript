c815e6db34d2e3dcd980563903911342da4e80c4
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-11-20 10:54:01 -0300

    script to calculate the area

--- calcPainting/js/index.js
+++ calcPainting/js/index.js
@@ -1,3 +1,12 @@
-function paint() {
-  
+// Script to calculate the area and amount of paint needed for painting
+
+let width = document.querySelector('.width')
+let height = document.querySelector('.height')
+let calculate = document.querySelector('.calculate')
+
+function calcPaint() {
+  let wid = Number(width.value)
+  let hei = Number(height.value)
+
+  let calc = wid * hei
 }
