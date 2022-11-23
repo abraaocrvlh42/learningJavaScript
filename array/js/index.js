c28085b7f760bf4418982f24ce7814a9b2e81ae4
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-11-22 22:00:39 -0300

    Examples of arrays in JavaScript

---
+++ array/js/index.js
@@ -0,0 +1,8 @@
+const fruits = ["Apple", "Banana", "Strawberry", "Mango", "Cherry"];
+const start = 2;
+const removedItems = fruits.splice(start);
+
+console.log(fruits);
+// ["Apple", "Banana"]
+console.log(removedItems);
+// ["Strawberry", "Mango", "Cherry"]
