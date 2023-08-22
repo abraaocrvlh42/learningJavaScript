5fa938dd11b99f476fb202ae1a2864d23f13f59b
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-08-21 21:39:22 -0300

    chess board exercise

--- consoleExercises/chess.js
+++ consoleExercises/chess.js
@@ -1,2 +1,20 @@
-function chess() {
+function createBoard(size) {
+  let board = '';
+
+  for (let line = 0; line < size; line++) {
+    for (let column = 0; column < size; column++) {
+      if ((line + column) % 2 === 0) {
+        board += ' ';
+      } else {
+        board += '#';
+      }
+    }
+    board += '\n';
+  }
+
+  return board;
 }
+
+const boardSize = 8;
+const boardChess = createBoard(boardSize);
+console.log(boardChess);
