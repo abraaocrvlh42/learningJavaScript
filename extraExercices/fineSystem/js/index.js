5daa2d352fbb72243ef1322bf31eda2a0e7f55ac
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-08-26 22:57:07 -0300

    fineSystem exercise

--- extraExercices/fineSystem/js/index.js
+++ extraExercices/fineSystem/js/index.js
@@ -1,6 +1,6 @@
 function calculate() {
   let vel = document.getElementById('.velocity')
-  let res = document.getElementById('result')
+  let res = document.getElementById('.result')
   let calc = Number(vel.value)
 
   result.innerHTML = `Your current speed is: ${vel}km/h`
