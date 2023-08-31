91e929ac4fc72e0be1997ac7f3142154e6f38556
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-08-30 22:29:01 -0300

    Examples of Arrays in JavaScript

--- examples/arrays.js
+++ examples/arrays.js
@@ -7,7 +7,7 @@ const animals = [
 ]
 
 // Getting specific positions within an array
-//console.log(animals[2])
+console.log(animals[2])
 
 // Using "length" to know how many elements are inside the array
 console.log(animals.length)
